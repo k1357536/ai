f1d092afa1a248a2bc2e1cdbd11b96d6a038b6f3
Michael Hansal <michael.hansal@gmail.com>
2015-11-02 00:30:32 +0100

    Theory done

--- java/src/at/jku/cp/ai/tests/assignment1/TestUninformedSearchers.java
+++ java/src/at/jku/cp/ai/tests/assignment1/TestUninformedSearchers.java
@@ -32,68 +32,54 @@ import at.jku.cp.ai.utils.PathUtils;
 import at.jku.cp.ai.utils.TestUtils;
 
 @RunWith(Parameterized.class)
-public class TestUninformedSearchers
-{
+public class TestUninformedSearchers {
 	@Parameters
-	public static Collection<Object[]> generateParams()
-	{
+	public static Collection<Object[]> generateParams() {
 		List<Object[]> params = new ArrayList<Object[]>();
-		for (int i = 0; i < Constants.NUMBER_OF_LEVELS; i++)
-		{
+
+		for (int i = 0; i < Constants.NUMBER_OF_LEVELS; i++) {
 			params.add(new Object[] { new Integer(i) });
 		}
+		params.add(new Object[] { new Integer(101) });
 		return params;
 	}
-	
+
 	@Rule
 	public Timeout timeout = new Timeout(40000);
 
-
 	private String pathToLevel;
 
-	public TestUninformedSearchers(Integer i)
-	{
+	public TestUninformedSearchers(Integer i) {
 		pathToLevel = String.format(Constants.ASSET_PATH + "/assignment1/L%d", i);
 	}
 
 	@Test
-	public void testBFS() throws Exception
-	{
-		testSearcherForLevel(
-				Board.fromLevelFile(pathToLevel + "/level"),
-				PathUtils.fromFile(pathToLevel + "/bfs.path"),
-				IBoardNode.class,
-				new BFS());
+	public void testBFS() throws Exception {
+		testSearcherForLevel(Board.fromLevelFile(pathToLevel + "/level"), PathUtils.fromFile(pathToLevel + "/bfs.path"),
+				IBoardNode.class, new BFS());
 	}
 
 	@Test
-	public void testIDS() throws Exception
-	{
+	public void testIDS() throws Exception {
 		List<V> pathToGoal = PathUtils.fromFile(pathToLevel + "/bfs.path");
-		testSearcherForLevel(
-				Board.fromLevelFile(pathToLevel + "/level"),
-				pathToGoal,
-				AlwaysMoveNode.class,
+		testSearcherForLevel(Board.fromLevelFile(pathToLevel + "/level"), pathToGoal, AlwaysMoveNode.class,
 				new IDS(pathToGoal.size()));
 
 	}
 
 	@Test
-	public void testDFS() throws Exception
-	{
-		testSearcherForLevel(
-				Board.fromLevelFile(pathToLevel + "/level"),
-				PathUtils.fromFile(pathToLevel + "/dfs.path"),
-				AlwaysMoveNode.class,
-				new DLDFS(40));
+	public void testDFS() throws Exception {
+		testSearcherForLevel(Board.fromLevelFile(pathToLevel + "/level"), PathUtils.fromFile(pathToLevel + "/dfs.path"),
+				AlwaysMoveNode.class, new DLDFS(40));
 	}
 
-	private void testSearcherForLevel(IBoard board, List<V> expectedPath, Class<?> nodeClazz, Search searcher) throws Exception
-	{
+	private void testSearcherForLevel(IBoard board, List<V> expectedPath, Class<?> nodeClazz, Search searcher)
+			throws Exception {
 		IBoard startBoard = board.copy();
 		final Fountain end = board.getFountains().get(0);
 
-		Predicate<Node> endReached = new IBoardPredicate(b -> b.isRunning() && b.getCurrentUnicorn().pos.equals(end.pos));
+		Predicate<Node> endReached = new IBoardPredicate(
+				b -> b.isRunning() && b.getCurrentUnicorn().pos.equals(end.pos));
 
 		List<Move> expectedMoveSequence = PathUtils.vsToMoves(expectedPath);
 		List<IBoard> expectedBoardStates = PathUtils.movesToIBoards(expectedMoveSequence, board.copy());
@@ -101,8 +87,7 @@ public class TestUninformedSearchers
 		Node startNode = (Node) nodeClazz.getDeclaredConstructor(IBoard.class).newInstance(startBoard);
 		Node endNode = searcher.search(startNode, endReached);
 
-		if (null == endNode)
-		{
+		if (null == endNode) {
 			fail("goal not found!");
 		}
 
