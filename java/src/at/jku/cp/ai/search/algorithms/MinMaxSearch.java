3e16ced0a68d4518e1d2ad3f00b9546184e110fb
Michael Hansal <michael.hansal@gmail.com>
2015-11-27 00:09:32 +0100

    MinMax working

--- java/src/at/jku/cp/ai/search/algorithms/MinMaxSearch.java
+++ java/src/at/jku/cp/ai/search/algorithms/MinMaxSearch.java
@@ -1,5 +1,6 @@
 package at.jku.cp.ai.search.algorithms;
 
+import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 
@@ -8,30 +9,60 @@ import at.jku.cp.ai.search.Node;
 import at.jku.cp.ai.search.datastructures.Pair;
 
 public class MinMaxSearch implements AdversarialSearch {
-
-	@SuppressWarnings("unused")
 	private Function<Node, Double> boardEvaluationFunction;
-	@SuppressWarnings("unused")
 	private BiPredicate<Integer, Node> searchLimitingPredicate;
-	@SuppressWarnings("unused")
-	private Node best;
-	@SuppressWarnings("unused")
-	private double bestValue;
 
 	/**
 	 * To limit the extent of the search, this implementation should honor a
-	 * limiting predicate. The predicate returns 'true' as long as we are below the limit,
-	 * and 'false', if we exceed the limit.
+	 * limiting predicate. The predicate returns 'true' as long as we are below
+	 * the limit, and 'false', if we exceed the limit.
 	 * 
 	 * @param searchLimitingPredicate
 	 */
-	public MinMaxSearch(BiPredicate<Integer, Node> slp)
-	{
+	public MinMaxSearch(BiPredicate<Integer, Node> slp) {
 		this.searchLimitingPredicate = slp;
 	}
 
 	public Pair<Node, Double> search(Node start, Function<Node, Double> evalFunction) {
-		// TODO: implement minmax search here
-		return new Pair<Node, Double>(start, 0d);
+		boardEvaluationFunction = evalFunction;
+
+		double best = Double.NEGATIVE_INFINITY;
+		Node result = start;
+		for (Node n : start.adjacent()) {
+			double value = minValue(n, 0);
+			if (value > best) {
+				best = value;
+				result = n;
+			}
+		}
+		return new Pair<Node, Double>(result, best);
+	}
+
+	private double maxValue(Node current, int level) {
+		double best = boardEvaluationFunction.apply(current);
+		if (searchLimitingPredicate.test(level, current)) {
+			double bestC = Double.NEGATIVE_INFINITY;
+			for (Node n : current.adjacent()) {
+				double value = minValue(n, level + 1);
+				if (value > bestC) {
+					best = bestC = value;
+				}
+			}
+		}
+		return best;
+	}
+
+	private double minValue(Node current, int level) {
+		double best = boardEvaluationFunction.apply(current);
+		if (searchLimitingPredicate.test(level, current)) {
+			double bestC = Double.POSITIVE_INFINITY;
+			for (Node n : current.adjacent()) {
+				double value = maxValue(n, level + 1);
+				if (value < bestC) {
+					best = bestC = value;
+				}
+			}
+		}
+		return best;
 	}
 }
