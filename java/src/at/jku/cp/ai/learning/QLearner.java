5153c137c0273a90e1ac0d3e2efa0e107b84e6a0
pybnen <pybnen@gmail.com>
2016-01-28 21:43:06 +0100

    Implement QLearner for basic test cases

--- java/src/at/jku/cp/ai/learning/QLearner.java
+++ java/src/at/jku/cp/ai/learning/QLearner.java
@@ -7,24 +7,33 @@ import java.util.Random;
 
 import at.jku.cp.ai.rau.IBoard;
 import at.jku.cp.ai.rau.objects.Move;
+import at.jku.cp.ai.rau.objects.V;
 import at.jku.cp.ai.search.datastructures.Pair;
 
 public class QLearner
 {
 	public Map<Pair<IBoard, Move>, Double> qmatrix;
-	@SuppressWarnings("unused")
-	private int numEpisodes;
-	@SuppressWarnings("unused")
-	private Random random;
-	@SuppressWarnings("unused")
-	private double discountFactor;
-	@SuppressWarnings("unused")
-	private boolean verbose;
-	@SuppressWarnings("unused")
-	private boolean bonus;
 
-	public QLearner(Random random, int numEpisodes, double discountFactor, boolean verbose)
-	{
+	private int numEpisodes;
+
+	private Random random;
+
+	private double discountFactor;
+
+	private boolean verbose;
+
+	private boolean bonus;
+	/** Contains position of the cloud */
+	private V cPos = null;
+	/** Contains position of spawned seed */
+	private V sPos = null;
+
+	/** reward for a good move */
+	private static double REWARD_GOOD_MOVE = 100d;
+	/** reward for a normal move */
+	private static double REWARD_NORMAL_MOVE = 0d;
+
+	public QLearner(Random random, int numEpisodes, double discountFactor, boolean verbose) {
 		this(random, numEpisodes, discountFactor, verbose, false);
 	}
 
@@ -38,10 +47,8 @@ public class QLearner
 	 *            this determines the importance of future rewards; in our test
 	 *            cases this plays a very minor role.
 	 */
-	public QLearner(Random random, int numEpisodes, double discountFactor, boolean verbose, boolean bonus)
-	{
-		if (discountFactor < 0d)
-		{
+	public QLearner(Random random, int numEpisodes, double discountFactor, boolean verbose, boolean bonus) {
+		if (discountFactor < 0d) {
 			throw new RuntimeException("discountFactor must be greater than 0.0!");
 		}
 
@@ -57,14 +64,119 @@ public class QLearner
 	 * This is the method that has to learn the qmatrix, which will be used later on!
 	 * @param board
 	 */
-	public void learnQFunction(IBoard board)
-	{
-		// HINT: adapt algorithm for q-learning from the lecture slides
-		// in the following way:
-		//
-		// for e in (0 .. number_of_episodes):
-		//     while not (goal reached):
-		//         ...
+	public void learnQFunction(IBoard board) {
+		// get position of cloud, there is only only cloud per board
+		cPos = board.getClouds().get(0).pos;
+		for (int e = 0; e < numEpisodes; e++) {
+			// start each episode with a fresh board
+			IBoard learnBoard = board.copy();
+			// reset seed position
+			sPos = null;
+			// learn as long as goal not reached and the unicorn is still on the board
+			while (!goalReached(learnBoard) && !wentSailing(learnBoard)) {
+				Move next = chooseMove(learnBoard);
+				IBoard prevBoard = learnBoard.copy();
+				learnBoard.executeMove(next);
+				// do not update qmatrix if unicorn went sailing
+				// because getPossibleMoves Method throws exception if no
+				// unicorn is on board
+				if (!wentSailing(learnBoard)) {
+					updateQMatrix(prevBoard, learnBoard, next);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Checks if unicorn is still on board
+	 * @param board
+	 * @return
+	 */
+	private boolean wentSailing(IBoard board) {
+		return board.getUnicorns().size() == 0;
+	}
+
+	/**
+	 * Updates qmatrix
+	 * @param prevBoard
+	 * @param newBoard
+	 * @param move
+	 */
+	private void updateQMatrix(IBoard prevBoard, IBoard newBoard, Move move) {
+		double reward = getReward(prevBoard, move);
+		qmatrix.put(new Pair<>(prevBoard, move), reward + discountFactor * getMaxScore(newBoard));
+	}
+
+	/**
+	 * Returns reward for given state and action
+	 * @param board
+	 * @param move
+	 * @return
+	 */
+	private double getReward(IBoard board, Move move) {
+		if (move == Move.SPAWN && nextToCloud(board)) {
+			// spawned seed next to cloud is a good thing -> good move
+			sPos = board.getUnicorns().get(0).pos;
+			return REWARD_GOOD_MOVE;
+		} else if (sPos != null) {
+			V uPos = board.getUnicorns().get(0).pos;
+			// seed is spawned, if unicorn is save from rainbow it is a good move
+			if ((sPos.x != uPos.x && sPos.y != uPos.y) ||	/* not in the same row/column -> save */
+					Math.abs(sPos.x - uPos.x) > 3 ||
+					Math.abs(sPos.y - uPos.y) > 3) { /* at least 3 tiles away from seed -> save */
+				// went away from seed -> good move
+				return REWARD_GOOD_MOVE;
+			}
+		}
+		return REWARD_NORMAL_MOVE;
+	}
+
+	/**
+	 * Checks if unicorn is next to cloud
+	 * @param board
+	 * @return
+	 */
+	private boolean nextToCloud(IBoard board) {
+		V uPos = board.getUnicorns().get(0).pos;
+		return (cPos.x == uPos.x && (cPos.y - 1 == uPos.y || cPos.y + 1 == uPos.y)) ||
+				(cPos.y == uPos.y && (cPos.x - 1 == uPos.x || cPos.x + 1 == uPos.x));
+	}
+
+	/**
+	 * Choose next move for learning
+	 * @param board
+	 * @return
+	 */
+	private Move chooseMove(IBoard board) {
+		List<Move> moves = board.getPossibleMoves();
+		// choose randomly from the possible moves
+		return moves.get(random.nextInt(moves.size()));
+	}
+
+	/**
+	 * Checks if end condition is reached
+	 * @param board
+	 * @return
+	 */
+	private boolean goalReached(IBoard board) {
+		return board.getEndCondition().getWinner() == 0;
+	}
+
+	/**
+	 * Returns current max score for a given state
+	 * @param board
+	 * @return
+	 */
+	public double getMaxScore(IBoard board) {
+		List<Move> moves = board.getPossibleMoves();
+		double bestScore = 0;
+		for (Move move : moves) {
+			double score = qmatrix.getOrDefault(new Pair<>(board, move), 0d);
+			if (score > bestScore) {
+				bestScore = score;
+			}
+		}
+		return bestScore;
 	}
 
 	/**
@@ -73,8 +185,7 @@ public class QLearner
 	 * @param board
 	 * @return
 	 */
-	public Move getMove(IBoard board)
-	{
+	public Move getMove(IBoard board) {
 		List<Move> moves = board.getPossibleMoves();
 
 		double bestScore = Double.NEGATIVE_INFINITY;
